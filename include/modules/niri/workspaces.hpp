33f791553423747b513d725e86ef9b180d123cc6
yuukibarns <yuukibarns@gmail.com>
2025-10-27 13:13:03 +0800

    .

--- include/modules/niri/workspaces.hpp
+++ include/modules/niri/workspaces.hpp
@@ -23,7 +23,8 @@ class Workspace {
 
  private:
   std::string getIcon(const std::string& value, const Json::Value& ws);
-  void updateTaskbar(const std::vector<Json::Value>& windows_data);
+  void updateTaskbar(const std::vector<Json::Value>& windows_data, const uint64_t active_window_id,
+                     const bool workspace_is_active);
 
   IconLoader iconLoader_;
   uint64_t id_;
